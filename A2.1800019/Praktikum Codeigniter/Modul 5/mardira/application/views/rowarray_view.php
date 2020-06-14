d1412f9bc801fbfb20e9f08f74210e2c33d96869
AndreasAprillian <andreasaprillian@gmail.com>
2020-06-15 01:32:43 +0700

    menyelesaikan modul codeigniter

---
+++ A2.1800019/Praktikum Codeigniter/Modul 5/mardira/application/views/rowarray_view.php
@@ -0,0 +1,11 @@
+<html>
+<head>
+<title>Metode Row Array</title>
+</head>
+<body>
+<h1>Metode Row Array</h1>
+<body>
+<?php echo "nama saya {$rowarray['nama_mhs']} dan NIM saya {$rowarray['NIM']}";
+?>
+</body>
+</html>
